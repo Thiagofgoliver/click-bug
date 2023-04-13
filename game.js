1d8a03149fc4adf7a012cc6e496163c660aadfaa
Thiago Felipe Guimarães oliveira <115421991+Thiagofgoliver@users.noreply.github.com>
2023-04-13 15:51:13 -0300

    jogo finalizado js

--- game.js
+++ game.js
@@ -80,11 +80,32 @@ const clickBug =(el)=>{
 
     let ponto = 10
          //se velocidade for maior que 20
-         //ponto vale 100 e mostra imagem "+100"
-         if(el.getAttribute('velocidade')){
+         //ponto vale 100 e mostra imagem "+100" (somente invasores)
+         if(el.getAttribute('velocidade') > 20 && el.classList.contains("invasor")){
 
             ponto = 100
+            //exibe a imagem +100 na posição do inseto
+           
+
+            //pontuação 100 ao matar mosquito
+            //desafio : mude o left eo top de img100 
+            //para o left top do "el"-css no js 
+            let img100 = document.getElementById ('pts100')
+            img100.style.left = el.style.left
+            img100.style.top= el.style.top
+            //muda apos 1/2 segundo muda o left de img100 para -'5000px'
+
+           setTimeout(()=>{
+
+            img100.style.left = el.style.left = '-5000px'
+           },350)
+
          }
+        // se elemento for "for bonzinho"
+        //pontuação vale -50
+        if(el.classList.contains('bonzinho')){
+           ponto = -50
+        }
 
 
         //soma na pontuação geral e remove da tela
@@ -139,7 +160,7 @@ for (const inv of invasores) {
     let incrementoinicio  = Math.floor(Math.random()*10+ 5)
     posicElemento(inv)
     moveElemento(inv,velocInicio,incrementoinicio)
-    inv.addEventListener('click',()=>{
+    inv.addEventListener('mousedown',()=>{
         clickBug (inv)
     })
 
@@ -156,12 +177,35 @@ for (const bom of bonzinhos) {
     posicElemento(bom)
     moveElemento(bom,velocInicio,incrementoinicio)
 
-    bom.addEventListener('click',()=>{
+    bom.addEventListener('mousedown',()=>{
         clickBug (bom)
+
+
     })
 }
+document.getElementById('infoTR').innerText = tempoRestante
+document.getElementById('temporest').innerText = tempoRestante
+//executa a cada segundo até atingir o valor 
+//da variavel tempoRestante SETINTERVAL
+//apos isso gameover 
+
+let tempo = tempoRestante
+const tempoGame = setInterval(()=>{
+    
+    
+    //mostra o tempo nos spans infoTR e temporest
+   
+
+    document.getElementById('infoTR').innerText = tempoRestante
+    document.getElementById('temporest').innerText = -- tempo
+    // se tempo for igual a 0, fim de jogo e recarrega a pagina
+    if(tempo == -1){
+        alert('GAMEOVER')
+        //recarregar a pagina (f5)
+        location.reload(true)
+    }
 
 
 
-
+},1000)
 
